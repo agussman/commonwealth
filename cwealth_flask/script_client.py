8c8527c295864115582a5d59ae0ee2a33142cb8f
Aaron Gussman <agussman@thehumangeo.com>
2015-08-09 12:34:01 -0400

    Modified WMATA API to use requests library and submit to AWS

--- cwealth_flask/script_client.py
+++ cwealth_flask/script_client.py
@@ -4,6 +4,11 @@ import simplejson
 import httplib
 import urllib
 from geopy.distance import great_circle
+import sys
+from pprint import pprint
+import time
+
+import requests
 
 keys = {
     'consumer_key': 'jb_FCNZnRQUl-ZBYIC7AMQ',
@@ -18,6 +23,8 @@ headers = {
     'api_key': '742e08c7b5324dfcb1a51cfbba70c8f8',
 }
 
+conn = httplib.HTTPSConnection('api.wmata.com')
+
 
 def main():
     # local lat & lon
@@ -25,6 +32,13 @@ def main():
     local_lat = 38.9048907
 
     json_file = client.search_by_geo_coord(latlong=(local_lat, local_lon), term="food", limit=20, radius=2000)
+
+    fout = open("yelp_results.json", "w")
+    json.dump(json_file, fout)
+    fout.close()
+
+
+    f = open("sample_data.json", "w")
 
     for i in json_file["businesses"]:
         bus_lat = json_file["region"]["center"]["latitude"]
@@ -41,67 +55,94 @@ def main():
             "transportation_score": compute_transportation_score(bus_lat, bus_lon)
         }
 
-    f = open("sample_data.json", "w")
-    json.dump(new_json, f)
+        #transportation_score = compute_transportation_score(bus_lat, bus_lon)
+
+
+        #pprint(new_json)
+        time.sleep(1)
+        #sys.exit(22)
+
+#    f = open("sample_data.json", "w")
+        #json.dump(new_json, f)
+
+        # push to ENDPOINT
+        endpoint = "http://52.2.225.70:8000/businesses/"
+        r = requests.post(endpoint, data=new_json)
+        print r
+        print r.text
+        #sys.exit(22)
+
     f.close()
 
 
 def compute_transportation_score(lat, lon):
-
-    train_data = distance_to_trains_json(lat, lon)
-    bus_date = distance_to_buses_json(lat, lon)
-    return smallest_distance(lat, lon, bus_date, train_data)
+    print "Lat: %s, Lon: %s" % (lat, lon)
+    train_dist = distance_to_trains_json(lat, lon)
+    #print train_dist
+    bus_dist = distance_to_buses_json(lat, lon)
+    #print bus_dist
+    #sys.exit(22)
+    #return smallest_distance(lat, lon, bus_data, train_data)
+    return min(train_dist, bus_dist)
 
 
 # Provides the distance from the small business to public transportation
 # Input: Requires the lat and long of the business
 # Output: Returns a float for the shortest distance to public transportation
 def distance_to_buses_json(lat, lon):
-    params = urllib.urlencode({
-        # Request parameters
+
+    endpoint = "https://api.wmata.com//Bus.svc/json/jStops"
+
+    params = {
         'Lat': lat,
         'Lon': lon,
-        # Hardcoded, can be changed
-        'Radius': '250',
-    })
+        'Radius': '1000',
+    }
 
-    conn = httplib.HTTPSConnection('api.wmata.com')
-    endpoint = "/Bus.svc/json/jStops?&%s" % params
-    conn.request("GET", endpoint, "{body}", headers)
-    response = conn.getresponse()
+    r = requests.get(endpoint, headers=headers, params=params)
+    #print r.text
 
-    print response.read()
+    temp_array = []
+    for i in r.json()["Stops"]:
+        temp_array.append(distance_calculator_two_coord(lat, lon, i["Lat"], i["Lon"]))
 
-    data = json.loads(response.read())
+    print min(temp_array)
 
-    conn.close()
+    #sys.exit(33)
 
-    return data
+    return min(temp_array)
 
 
 def distance_to_trains_json(lat, lon):
-    params = urllib.urlencode({
-        # Request parameters
+
+    #endpoint = "https://api.wmata.com//Bus.svc/json/jStops"
+    endpoint = "https://api.wmata.com/Rail.svc/json/jStationEntrances"
+
+    params = {
         'Lat': lat,
         'Lon': lon,
-        # Hardcoded, can be changed
-        'Radius': '250',
-    })
+        'Radius': '1000',
+    }
 
-    conn = httplib.HTTPSConnection('api.wmata.com')
-    endpoint = "/Rail.svc/json/jStationEntrances?&%s" % params
-    conn.request("GET", endpoint, "{body}", headers)
-    response = conn.getresponse()
+    r = requests.get(endpoint, headers=headers, params=params)
+    print r.text
 
-    print response.read()
+    #data = simplejson.loads(response.read())
+#    conn.close()
 
-    data = simplejson.loads(response.read())
+    #pprint(data)
 
 
+    temp_array = []
+    #for i in data["Entrances"]:
+    for i in r.json()["Entrances"]:
+        temp_array.append(distance_calculator_two_coord(lat, lon, i["Lat"], i["Lon"]))
 
-    conn.close()
+    print min(temp_array)
 
-    return data
+    #sys.exit(22)
+
+    return min(temp_array)
 
 
 def distance_calculator_two_coord(business_lat, business_lon, bus_stop_lat, bus_stop_lon):
