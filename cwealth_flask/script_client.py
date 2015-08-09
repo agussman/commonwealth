d37dc7102fe94a6689c6a5997e7c3efb24ab99a7
Jaime5 <Jaime5@UMBC.edu>
2015-08-09 04:03:17 -0400

    Finding Smallest Distance

--- cwealth_flask/script_client.py
+++ cwealth_flask/script_client.py
@@ -11,63 +11,49 @@ keys = {
     'token_secret': 'PHG1XzFh0r5ZVQpDt74VN-TN5bs',
 }
 
+client = YelpClient(keys)
+
 headers = {
-    # Request headers
     'api_key': '742e08c7b5324dfcb1a51cfbba70c8f8',
 }
 
-client = YelpClient(keys)
-
 
 def main():
-    lat = 38.9048907
-    lon = -77.0339064
+    # local lat & lon
 
-    json_file = provide_stores_json(lat, lon)
+    local_lon = -77.0339064
+    local_lat = 38.9048907
 
-    # print "Name:", json_file["businesses"][0]["name"]
-    # print "ID:", json_file["businesses"][0]["id"]
-    # print "Image URL:", json_file["businesses"][0]["image_url"]
-    # print "Phone Number:", json_file["businesses"][0]["phone"]
-    # print json_file["businesses"][0]["location"]["display_address"][0]
-    # print json_file["businesses"][0]["location"]["display_address"][1]
-    # print json_file["businesses"][0]["location"]["display_address"][2]
+    json_file = client.search_by_geo_coord(latlong=(local_lat, local_lon), term="food", limit=1, radius=2000)
+
+    print "Name:", json_file["businesses"][0]["name"]
+    print "ID:", json_file["businesses"][0]["id"]
+    print "Image URL:", json_file["businesses"][0]["image_url"]
+    print "Phone Number:", json_file["businesses"][0]["phone"]
+    print json_file["businesses"][0]["location"]["display_address"][0]
+    print json_file["businesses"][0]["location"]["display_address"][1]
+    print json_file["businesses"][0]["location"]["display_address"][2]
 
     lat = json_file["region"]["center"]["latitude"]
     lon = json_file["region"]["center"]["longitude"]
 
-    f = open("food_search.json", "w")
+    f = open("restaurant_search.json", "w")
     json.dump(json_file, f)
     f.close()
 
-    distances = distance_to_buses_json(lat, lon)
-
-    for i in distances["Stops"]:
-        print (distance_calculator_two_coord(lat, lon, i["Lat"], i["Lon"]))
-
-    j = open("bus_information.json", "w")
-
-    json.dump(distances, j)
-
-    j.close()
-
-
-# Input: Lat, Long of the person using the app
-# Output: Json file of stores
-def provide_stores_json(lat, lon):
-    return client.search_by_geo_coord(latlong=(lat, lon), term="food", limit=1, radius=2000)
+    print smallest_distance(lat, lon, distance_to_buses_json(lat, lon), distance_to_trains_json(lat, lon))
 
 
 # Provides the distance from the small business to public transportation
 # Input: Requires the lat and long of the business
-# Output: Returns a float for the shortest distance to public tranportation
+# Output: Returns a float for the shortest distance to public transportation
 def distance_to_buses_json(lat, lon):
     params = urllib.urlencode({
         # Request parameters
         'Lat': lat,
         'Lon': lon,
         # Hardcoded, can be changed
-        'Radius': '500',
+        'Radius': '250',
     })
 
     conn = httplib.HTTPSConnection('api.wmata.com')
@@ -87,7 +73,7 @@ def distance_to_trains_json(lat, lon):
         'Lat': lat,
         'Lon': lon,
         # Hardcoded, can be changed
-        'Radius': '1000',
+        'Radius': '500',
     })
 
     conn = httplib.HTTPSConnection('api.wmata.com')
@@ -103,10 +89,20 @@ def distance_to_trains_json(lat, lon):
 
 def distance_calculator_two_coord(business_lat, business_lon, bus_stop_lat, bus_stop_lon):
     first = (business_lat, business_lon)
-
     second = (bus_stop_lat, bus_stop_lon)
-
     return great_circle(first, second).miles
+
+
+def smallest_distance(business_lat, business_lon, bus_data, train_data):
+    temp_array = []
+
+    for i in bus_data["Stops"]:
+        temp_array.append(distance_calculator_two_coord(business_lat, business_lon, i["Lat"], i["Lon"]))
+
+    for i in train_data["Entrances"]:
+        temp_array.append(distance_calculator_two_coord(business_lat, business_lon, i["Lat"], i["Lon"]))
+
+    return min(temp_array)
 
 
 if __name__ == "__main__":
