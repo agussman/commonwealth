fe1f78d429501d79ecf1c9d3beb2ec22d4c359fc
Aaron Gussman <agussman@thehumangeo.com>
2015-08-08 23:53:38 -0400

    Corrected typo in bus_api.py (missing '?' is URL query)

--- communityBuying/bus_api.py
+++ communityBuying/bus_api.py
@@ -4,22 +4,25 @@ import httplib, urllib, base64
 
 #-API KEY- 742e08c7b5324dfcb1a51cfbba70c8f8
 
+#curl -v -X GET "https://api.wmata.com/Bus.svc/json/jStops?Lat=38.8995873500&Lon=-77.02773&Radius=3" -H "api_key: 742e08c7b5324dfcb1a51cfbba70c8f8"
+
+
 headers = {
     # Request headers
-    'api_key': '{subscription key}',
+    'api_key': '742e08c7b5324dfcb1a51cfbba70c8f8',
 }
 
 params = urllib.urlencode({
     # Request parameters
-    'Lat': '{number}',
-    'Lon': '{number}',
-    'Radius': '{number}',
+    'Lat': '38.89958735000000',
+    'Lon': '-77.02773315',
+    'Radius': '1000',
 })
 
 try:
     conn = httplib.HTTPSConnection('api.wmata.com')
-    endpoint = "/Bus.svc/json/jStops&%s" % params
-    print conn
+    endpoint = "/Bus.svc/json/jStops?&%s" % params
+    #print (endpoint)
 
 
     conn.request("GET", endpoint, "{body}", headers)
