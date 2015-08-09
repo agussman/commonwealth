8c4eac7d561a296921f12910af02b5e4adffaa90
Jaime5 <Jaime5@UMBC.edu>
2015-08-08 21:30:18 -0400

    Edited

--- communityBuying/script.py
+++ communityBuying/script.py
@@ -1,42 +1,43 @@
 import json
-
 import rauth
 
 
 def main():
+
     json_data = get_results(get_search_parameters(38.9048907, -77.0339064))
 
-    with open('our_location.json', 'r') as handle:
-        parsed = json.load(handle)
 
-    print "Name:", parsed["businesses"][0]["name"]
-    print "ID:", parsed["businesses"][0]["id"]
-    print "Image URL:", parsed["businesses"][0]["image_url"]
-    print "Phone Number:", parsed["businesses"][0]["phone"]
-    print parsed["businesses"][0]["location"]["display_address"][0]
-    print parsed["businesses"][0]["location"]["display_address"][1]
-    print parsed["businesses"][0]["location"]["display_address"][2]
+    print "Name:", json_data["businesses"][0]["name"]
+    print "ID:", json_data["businesses"][0]["id"]
+    print "Image URL:", json_data["businesses"][0]["image_url"]
+    print "Phone Number:", json_data["businesses"][0]["phone"]
+    print json_data["businesses"][0]["location"]["display_address"][0]
+    print json_data["businesses"][0]["location"]["display_address"][1]
+    print json_data["businesses"][0]["location"]["display_address"][2]
 
 
 # Requests to Yelp API - Function
 # Input: parameters to call API Function
 # Output: json Query
 def get_results(params):
-    # Obtain API Keys from Yelp's manage access page
-    # Commence Session
+
+    # Obtain these from Yelp's manage access page
+    consumer_key = "jb_FCNZnRQUl-ZBYIC7AMQ"
+    consumer_secret = "9w2belyaG3TQljVJ3AFqAct2zsQ"
+    token = "nwmC3CUV1Gq8W8idtT2SIhVsPEqYVVhj"
+    token_secret = "PHG1XzFh0r5ZVQpDt74VN-TN5bs"
+
     session = rauth.OAuth1Session(
-        consumer_key="jb_FCNZnRQUl-ZBYIC7AMQ",
-        consumer_secret="9w2belyaG3TQljVJ3AFqAct2zsQ",
-        access_token="nwmC3CUV1Gq8W8idtT2SIhVsPEqYVVhj",
-        access_token_secret="PHG1XzFh0r5ZVQpDt74VN-TN5bs")
+        consumer_key=consumer_key
+        , consumer_secret=consumer_secret
+        , access_token=token
+        , access_token_secret=token_secret)
 
-    # Requesting + Storing
-    request = session.get("http://api.yelp.com/v2/business", params=params)
+    request = session.get("http://api.yelp.com/v2/search", params=params)
 
-    # Transforms response into a JSON Format
+    # Transforms the JSON API response into a Python dictionary
     data = request.json()
 
-    # Session Closed
     session.close()
 
     return data
